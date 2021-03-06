dec2349a55888975686248627a1dacb19d556082
Phil McCarthy <philmcc2631@gmail.com>
2021-03-06 10:13:44 -0700

    Add gem config to rails & spec helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,3 +73,10 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.configure_rspec_metadata!
+  config.filter_sensitive_data('<MAP_QUEST_KEY>') { ENV['MAP_QUEST_KEY'] }
+end
