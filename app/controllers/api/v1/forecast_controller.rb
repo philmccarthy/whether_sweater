59c5e801e6deba8fd64d77f006bf20aec12fb9c7
Phil McCarthy <philmcc2631@gmail.com>
2021-03-07 11:33:49 -0700

    Add forecast facade and service

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,22 +1,8 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     location = GeocodeFacade.get_geocode(params[:location])
-
-    conn = Faraday.new("https://api.openweathermap.org/data/2.5/") do |request|
-      request.params[:appid] = ENV['OPEN_WEATHER_KEY']
-    end
-
-    response = conn.get("onecall") do |request|
-      request.params[:lat] = location.lat
-      request.params[:lon] = location.lng
-      request.params[:units] = 'imperial'
-      request.params[:exclude] = 'minutely,alerts'
-    end
-
-    parsed = JSON.parse(response.body, symbolize_names: true)
-
-    forecast = Forecast.new(parsed)
-
-    render json: ForecastSerializer.new(forecast)
+    render json: ForecastSerializer.new(
+      ForecastFacade.get_forecast(location)
+    )
   end
 end
