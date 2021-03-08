eb1a0c534b2de51b647522d883220d00668d23c7
Phil McCarthy <philmcc2631@gmail.com>
2021-03-08 10:00:05 -0700

    Add travel_time offset to Yelp restaurant search

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,30 +1,38 @@
 class Api::V1::MunchiesController < ApplicationController
   def index
-    # Restaurant - Yelp Fusion business search
-    conn = Faraday.new('https://api.yelp.com/v3/businesses/') do |request|
-      request.headers[:authorization] = "Bearer #{ENV['YELP_KEY']}"
-    end
-    
-    response = conn.get('search') do |request|
-      request.params[:location] = params[:destination]
-      request.params[:term] = params[:food]
-    end
-
-    parsed = JSON.parse(response.body, symbolize_names: true)
-    restaurant = parsed[:businesses][0]
     # Travel time - MQ Route
-
     response = Faraday.get('http://www.mapquestapi.com/directions/v2/route') do |request|
       request.params[:key] = ENV['MAP_QUEST_KEY']
       request.params[:from] = params[:start]
       request.params[:to] = params[:destination]
     end
-
     parsed = JSON.parse(response.body, symbolize_names: true)
     travel_time = parsed[:route][:formattedTime]
 
     # Current forecast - Open Weather
-    forecast = ForecastFacade.get_forecast(params[:destination])
-    require 'pry'; binding.pry
+    destination = GeocodeFacade.get_geocode(params[:destination])
+    weather_data = ForecastFacade.get_forecast(destination)
+    forecast = {
+      summary: weather_data.current_weather[:conditions],
+      temperature: weather_data.current_weather[:temp]
+    }
+
+    # Restaurant - Yelp Fusion business search
+    
+    conn = Faraday.new('https://api.yelp.com/v3/businesses/') do |request|
+      request.headers[:authorization] = "Bearer #{ENV['YELP_KEY']}"
+    end
+    
+    travel_time_offset = travel_time.split(':')
+    arrival_time = Time.now + travel_time_offset[0].to_i.hours + travel_time_offset[1].to_i.minutes
+
+    response = conn.get('search') do |request|
+      request.params[:location] = params[:destination]
+      request.params[:term] = params[:food]
+      request.params[:open_at] = arrival_time.to_i
+    end
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+    restaurant = parsed[:businesses][0]
   end
 end
