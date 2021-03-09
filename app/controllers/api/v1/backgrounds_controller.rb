9303b170c623dbcc13a8a732e98efee70f0140a4
Phil McCarthy <philmcc2631@gmail.com>
2021-03-08 18:35:29 -0700

    Add whitespace

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
     raise Exceptions::InvalidParams unless params[:location].present?
+    
     render json: ImageSerializer.new(
       BackgroundsFacade.get_background(params[:location])
     )
